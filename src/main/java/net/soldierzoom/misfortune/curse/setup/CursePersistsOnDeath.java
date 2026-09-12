7ee0b1b8d79a42eeb2823a3a5843d575ef43391a
SoldierZoom <73135242+SoldierZoom@users.noreply.github.com>
2026-09-12 02:09:41 +0100

    fixed missing capability bug + added client syncing

--- src/main/java/net/soldierzoom/misfortune/curse/setup/CursePersistsOnDeath.java
+++ src/main/java/net/soldierzoom/misfortune/curse/setup/CursePersistsOnDeath.java
@@ -1,16 +1,35 @@
 package net.soldierzoom.misfortune.curse.setup;
 
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.soldierzoom.misfortune.curse.capability.PlayerCurse;
 import net.soldierzoom.misfortune.curse.main.ICurse;
+import net.soldierzoom.misfortune.network.ModNetwork;
+import net.soldierzoom.misfortune.network.S2C_CurseSyncPacket;
 
 public class CursePersistsOnDeath {
+    //server side
     @SubscribeEvent
     public void onClone(PlayerEvent.Clone e) {
         // Copy old -> new
+        e.getOriginal().reviveCaps();
+
         ICurse oldC = PlayerCurse.get(e.getOriginal());
         ICurse newC = PlayerCurse.get(e.getEntity());
+
         newC.set(oldC.get());
+
+        e.getOriginal().invalidateCaps();
+    }
+    //sync client
+    @SubscribeEvent
+    public void onRespawn(PlayerEvent.PlayerRespawnEvent e) {
+        if (!(e.getEntity() instanceof ServerPlayer sp)) return;
+
+        ModNetwork.sendToClient(
+                new S2C_CurseSyncPacket(PlayerCurse.get(sp).get()),
+                sp
+        );
     }
 }
